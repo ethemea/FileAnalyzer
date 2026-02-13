92cffc10ee7baaa0caea6025c55d144713725271
casde3661 <casde3661@gmail.com>
2026-02-14 00:43:39 +0700

    main is clean

--- Analyzer.java
+++ Analyzer.java
@@ -1,85 +1,14 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
  
 public class Analyzer {
  
     public static void main(String[] args) throws IOException { 
-        boolean shortStat = false;
-        boolean fullStat = false;
-        boolean addData = false;
-        ArrayList<String> array = new ArrayList<String>();
-        String path = null;
-        String filename = null;
-
-        ArrayList<String> files = new ArrayList<String>();
-        int len = args.length;
-        for (int i = 0; i < len; i++) {
-            if (args[i].endsWith(".txt")) {
-                File f = new File(args[i]);
-                if(f.exists() && !f.isDirectory()) { 
-                    files.add(args[i]);
-                }
-            } else switch (args[i]) {
-                case "-s":
-                    shortStat = true;
-                    break;
-                case "-f":
-                    fullStat = true;
-                    break;
-                case "-a":
-                    addData = true;
-                    break;
-                case "-p":
-                    if (i + 1 < len) {
-                        if (!(args[i+1].endsWith(".txt"))) {
-                            filename = args[i+1];
-                            i++;
-                        }
-                    }
-                    break;
-                case "-o":
-                    if (i + 1 < len) {
-                        if (!(args[i+1].endsWith(".txt"))) {
-                            path = args[i+1];
-                            i++;
-                        }
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-        
-        for (String filePath : files) {
-            Scanner sc = new Scanner(new FileReader(filePath)).useDelimiter("\n"); // TODO перенос в класс Sorter ??
-            String str;
-            while (sc.hasNext()) { 
-                str = sc.next(); 
-                String regex = "[\\p{C}]"; 
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(str);
-                String cleanStr = matcher.replaceAll("");
-                array.add(cleanStr);
-            }
-            if (sc != null) {
-                sc.close();
-            }
-        }
-      
-        Sorter sorter = new Sorter(array);
+        Sorter sorter = new Sorter();
+        sorter.readArgs(args);
+        sorter.readFiles();
         sorter.sort();
-        sorter.output(addData, path, filename);
-        if (fullStat || (shortStat && fullStat)) {
-            sorter.fullStat();
-        } 
-        if (shortStat) { 
-            sorter.shortStat(); 
-        }
-        
+        sorter.output();        
     }
 
 }
--- Sorter.java
+++ Sorter.java
@@ -1,11 +1,21 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Sorter {
+    private boolean shortStat = false;
+    private boolean fullStat = false;
+    private boolean addData = false;
+    private String path = null;
+    private String filename = null;
     private Integer numberOfStrings;
     private Integer numberOfIntegers;
     private Integer numberOfFloats;
@@ -17,19 +27,83 @@ public class Sorter {
     private Float floatSum;
     private Integer strMin;
     private Integer strMax;
-    private ArrayList<String> array;
+    private ArrayList<String> array = new ArrayList<String>();
+    private ArrayList<String> files = new ArrayList<String>();
     private ArrayList<String> arrayOfStrings = new ArrayList<>();
     private ArrayList<String> arrayOfIntegers = new ArrayList<>();
     private ArrayList<String> arrayOfFloats = new ArrayList<>();
     private static final String[] illegalChars = { "/", "\n", "\r", "\t", "\0", "\f", "`", "\\?", "\\*", "\\\\", "<", ">", "|", "\"", ":" };
-    public Sorter(ArrayList<String> array) {
+
+    public Sorter() {
         this.numberOfStrings = 0;
         this.numberOfIntegers = 0;
         this.numberOfFloats = 0;
         this.floatSum = Float.valueOf(0);
         this.intSum = Long.valueOf(0);
-        this.array = array;
     }
+
+    public void readArgs(String[] args) {
+        int len = args.length;
+        for (int i = 0; i < len; i++) {
+            if (args[i].endsWith(".txt")) {
+                File f = new File(args[i]);
+                if(f.exists() && !f.isDirectory()) { 
+                    files.add(args[i]);
+                }
+            } else switch (args[i]) {
+                case "-s":
+                    shortStat = true;
+                    break;
+                case "-f":
+                    fullStat = true;
+                    break;
+                case "-a":
+                    addData = true;
+                    break;
+                case "-p":
+                    if (i + 1 < len) {
+                        if (!(args[i+1].endsWith(".txt"))) {
+                            filename = args[i+1];
+                            i++;
+                        }
+                    }
+                    break;
+                case "-o":
+                    if (i + 1 < len) {
+                        if (!(args[i+1].endsWith(".txt"))) {
+                            path = args[i+1];
+                            i++;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void readFiles() {
+        for (String filePath : files) {
+            try {
+                Scanner sc = new Scanner(new FileReader(filePath)).useDelimiter("\n");
+                String str;
+                while (sc.hasNext()) { 
+                    str = sc.next(); 
+                    String regex = "[\\p{C}]"; 
+                    Pattern pattern = Pattern.compile(regex);
+                    Matcher matcher = pattern.matcher(str);
+                    String cleanStr = matcher.replaceAll("");
+                    array.add(cleanStr);
+                }
+                if (sc != null) {
+                    sc.close();
+                }
+            } catch (FileNotFoundException ex) {
+                System.out.println("Input files were nor found: " + ex.getMessage());
+            }
+        }
+    }
+
     private static boolean isFloat(String str) {
         if (str == null) {
             return false;
@@ -41,6 +115,7 @@ public class Sorter {
         }
         return true;
     }   
+
     private static boolean isInteger(String str) {
         if (str == null) {
             return false;
@@ -52,6 +127,7 @@ public class Sorter {
         }
         return true;
     }   
+
     public void sort() {
         for (String str : array) {
             if (isInteger(str)) { 
@@ -104,14 +180,16 @@ public class Sorter {
             }
         }
     }
+
     private boolean pathCheck (String path) {
         try {
-        Paths.get(path);
+            Paths.get(path);
         } catch (InvalidPathException | NullPointerException ex) {
             return false;
         }
         return true;
     }
+
     private String nameCheck (String name) {
         if (name == null) {
             return "";
@@ -122,6 +200,7 @@ public class Sorter {
         }
         return name;
     }
+
     private File createFile(File theDir, String name) {
         File file;
         if (theDir != null) {
@@ -133,11 +212,13 @@ public class Sorter {
             file.createNewFile();
         } catch (IOException ex) {
             System.err.println("Failed to create file " + name + ": " + ex.getMessage());
+            return null;
         }
         return file;
     }
-    public void output(boolean a, String path, String name) throws IOException {
-        name = nameCheck(name); 
+
+    public void output() throws IOException {
+        filename = nameCheck(filename); 
         if (pathCheck(path)) {
             String absolutePath = Paths.get("").toAbsolutePath().toString();
             File theDir = new File(absolutePath + path);
@@ -146,25 +227,30 @@ public class Sorter {
             } catch (Exception ex) {
                 System.err.println("Failed to create directories: " + ex.getMessage());
             }
-            write(createFile(theDir, name + "integers.txt"), arrayOfIntegers, a);
-            write(createFile(theDir, name + "floats.txt"), arrayOfFloats, a);
-            write(createFile(theDir, name + "strings.txt"), arrayOfStrings, a);
+            write(createFile(theDir, filename + "integers.txt"), arrayOfIntegers);
+            write(createFile(theDir, filename + "floats.txt"), arrayOfFloats);
+            write(createFile(theDir, filename + "strings.txt"), arrayOfStrings);
         } else {
-            write(createFile(null, name + "integers.txt"), arrayOfIntegers, a);
-            write(createFile(null, name + "floats.txt"), arrayOfFloats, a);
-            write(createFile(null, name + "strings.txt"), arrayOfStrings, a);
+            write(createFile(null, filename + "integers.txt"), arrayOfIntegers);
+            write(createFile(null, filename + "floats.txt"), arrayOfFloats);
+            write(createFile(null, filename + "strings.txt"), arrayOfStrings);
+        }
+        if ((shortStat && fullStat) || fullStat) {
+            fullStat();
+        } else if (shortStat) {
+            shortStat();
         }
     }
-    private void write(File file, ArrayList<String> toWrite, boolean a) throws IOException {
+    private void write(File file, ArrayList<String> toWrite) throws IOException {
         if (toWrite.size() > 0) {
-            try (FileWriter writer = new FileWriter(file, a)) {
+            try (FileWriter writer = new FileWriter(file, addData)) {
                 for (String each : toWrite) {
                     writer.write(each + "\n");
                 }
                 writer.close();
             }
-            catch(IOException ex) {
-            System.out.println(ex.getMessage());
+            catch (IOException | NullPointerException ex) {
+                System.out.println("Failed to write output file");
             } 
         }
     }
